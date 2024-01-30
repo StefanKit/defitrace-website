7e767e4f3d9a25b2d778860b7a25db64ed663180
Stefan <kitzler@csh.ac.at>
2024-01-30 17:55:16 +0100

    mv CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: "http://defitrace.info/",
-  integrations: [tailwind(), mdx(), sitemap()],
-});
+  site: 'https://example.com',
+})
